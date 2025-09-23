a3ef9e962043ca41a5c3d4865dfddcd30c64cfaf
varshinivij <varshinivijay05@gmail.com>
2025-09-23 14:55:46 -0700

    styling + upload changes

--- matthew_rose_labs/src/App.jsx
+++ matthew_rose_labs/src/App.jsx
@@ -1,19 +1,45 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 function App() {
   const [image, setImage] = useState(null);
+  const [status, setStatus] = useState('Initial');
 
-  const handleUpload = (file) => {
-    if (file) {
-      setImage(URL.createObjectURL(file));
+  const handleFileChange = (e) => {
+    if (e.target.files) {
+      const file = e.target.files[0];
+      setStatus('Loading');
+      setImage(file);
+      handleUpload(file);
+    }
+  };
+
+  const handleUpload = async (file) => {
+    const formData = new FormData();
+    formData.append('file', file);
+    try {
+      const result = await fetch('http://localhost:5000/matthew_rose_labs', {
+        method: 'POST',
+        body: formData,
+      });
+
+      const data = await result.json();
+      setStatus('Completed');
+    } catch (error) {
+      setStatus('Error');
     }
   };
 
   return (
     <div>
       <p className='main-title'>Welcome to Matthew-Rose Labs</p>
-      <input type='file' onChange={(e) => handleUpload(e.target.files[0])} />
-      {image && <img src={image} alt="Uploaded" style={{ maxWidth: '300px', marginTop: '10px' }} />}
+      <input type='file' onChange={handleFileChange} />
+      {image && (
+        <img
+          src={URL.createObjectURL(image)}
+          style={{ maxWidth: '300px', marginTop: '10px' }}
+        />
+      )}
+      <p>{status}</p>
     </div>
   );
 }
