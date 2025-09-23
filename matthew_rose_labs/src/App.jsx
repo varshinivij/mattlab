9ce0963c39a40521196914f6ee03ec067ebdebdb
varshinivij <varshinivijay05@gmail.com>
2025-09-23 11:38:01 -0700

    working ver

--- matthew_rose_labs/src/App.jsx
+++ matthew_rose_labs/src/App.jsx
@@ -1,19 +1,21 @@
-import {useState, useEffect} from 'react';
+import { useState } from 'react';
 
 function App() {
-  const [image, setImage] = useState(None);
+  const [image, setImage] = useState(null);
 
   const handleUpload = (file) => {
     if (file) {
-      setImage(URL.createObjectURL(file))
+      setImage(URL.createObjectURL(file));
     }
-  }
+  };
 
   return (
     <div>
       <p className='main-title'>Welcome to Matthew-Rose Labs</p>
-      <input type='file' onClick={(e) => handleUpload(e.target.files[0])}> Upload File </input>
-      {image && <img src={image}></img>}
+      <input type='file' onChange={(e) => handleUpload(e.target.files[0])} />
+      {image && <img src={image} alt="Uploaded" style={{ maxWidth: '300px', marginTop: '10px' }} />}
     </div>
-  )
+  );
 }
+
+export default App;
