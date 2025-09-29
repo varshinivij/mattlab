3ca7e966327f0ca6970a89a6306b11595e44c5bd
varshinivij <varshinivijay05@gmail.com>
2025-09-29 13:46:19 -0700

    cors

--- matthew_rose_labs/backend/main.py
+++ matthew_rose_labs/backend/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI, File, Form, UploadFile, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 images = []
+
+origins = [
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/")
 async def add_image(fileName: str = Form(...), file: UploadFile = File(...)):
