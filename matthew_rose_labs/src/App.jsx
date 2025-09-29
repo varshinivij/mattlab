d49294d7dea9ace432e23d3b939919ed4f826cc9
varshinivij <varshinivijay05@gmail.com>
2025-09-28 17:49:47 -0700

    removed a useless folder - still have issues

--- matthew_rose_labs/src/App.jsx
+++ matthew_rose_labs/src/App.jsx
@@ -1,64 +1,63 @@
 import React, { useState } from 'react';
-import api from './api.jsx';  
 import axios from 'axios';
 
 const allowedFiles = ['.jpeg', '.jpg', '.png'];
 
 function App() {
   const [file, setFile] = useState(null);
-  const [status, setStatus] = useState('Initial');
+  const [status, setStatus] = useState('No File Chosen Yet');
   const [filename, setFileName] = useState('output.png');
 
-  function postRequest(newFile, newFileName) {
+  function postFileRequest() {
     const formData = new FormData();
-    //standard JS object for file uploads
-    formData.append("file", newFile);
-    formData.append("filename", newFileName);
-    axios.post('api', formData)
-      .then( (response) => setStatus(`File uploaded: ${response.data}`))
-      .catch( (error) => setStatus(`Error Encountered: ${error.message}`))
-  };
+    formData.append("file", file);
+    formData.append("fileName", filename);
+    axios.post('/api', formData)
+      .then(response => setStatus(`File uploaded: ${response.data}`))
+      .catch(error => setStatus(`Error Encountered: ${error.message}`));
+  }
 
-
-  const handleFileChange = (e) => {
+  const handleFileUpload = (e) => {
     if (e.target.files) {
       const file = e.target.files[0];
       if (file.size > 5000000) {
         setStatus("Error: File size larger than 5MB");
         return;
-      } else if (!file.type.startsWith('image/')){
+      } else if (!file.type.startsWith('image/')) {
         setStatus("Error: Upload an image");
         return;
       } else if (!allowedFiles.some(ext => file.name.toLowerCase().endsWith(ext))) {
         setStatus("Error: File type not supported - upload PNG, JPEG or JPG");
         return;
       }
-      setFile(file); 
-      setFileName(filename);
-      postRequest(file, filename);
+      setFile(file);
+      postFileRequest(file, filename);
     } else {
       setStatus("Error: Empty file");
     }
-    }
+  }
 
+  return (
+    <div>
+      <p className='main-title'>Welcome to Matthew-Rose Labs</p>
 
+      <label> Choose File:
+        <input type='file' onChange={handleFileUpload}/>
+      </label>
+      <label > FileName:
+        <input type='text' onKeyDown={(e => setFileName(e.target.value))}/>
+      </label>
+      <button type="submit" onClick={postFileRequest}>Submit</button>
 
-    return (
-      <div>
-        <p className='main-title'>Welcome to Matthew-Rose Labs</p>
-        <input type='file' onChange={handleFileChange} />
-        <input type='text' onkeypress={handleFileChange} />
-        {file && (
-          <img
-            src={URL.createObjectURL(file)}
-            style={{ maxWidth: '300px', marginTop: '10px' }}
-          />
-        )}
-        <p>{status}</p> 
-      </div>
-    );
-};
-
+      {file && (
+        <img
+          src={URL.createObjectURL(file)}
+          style={{ maxWidth: '300px', marginTop: '10px' }}
+        />
+      )}
+      <p>{status}</p>
+    </div>
+  );
+}
 
 export default App;
-
